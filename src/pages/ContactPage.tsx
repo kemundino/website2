0794d45641bf00d04f363d40be976d0364aa93d2
kemundino <kedirmundino05@gmail.com>
2026-05-23 23:55:18 +0300

    contact form update

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -52,7 +52,8 @@ const ContactPage = () => {
     <div className="relative min-h-[calc(100vh-4rem)] w-full overflow-hidden bg-background px-4 py-12 sm:px-6 lg:px-8">
       {/* Decorative background blobs */}
       <div className="pointer-events-none absolute left-1/2 top-0 -z-10 -translate-x-1/2 blur-3xl xl:-top-6 opacity-40">
-        <div className="aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-orange-400 to-rose-400" style={{ clipPath: 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)' }}></div>
+        <div className="aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-orange-400 to-rose-400" style={{ 
+          clipPath: 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)' }}></div>
       </div>
 
       <div className="mx-auto max-w-7xl">
